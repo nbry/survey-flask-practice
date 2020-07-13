a3f0db6420543b79af75f6d4b8a45ad4443c048e
nathan <nwbryant@umich.edu>
2020-07-13 14:38:30 -0700

    implemented question progression

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app = Flask(__name__)
 
 responses = []
 
-@app.route('/start')
-def start_page():
-    import pdb
-    pdb.set_trace()
-    return render_template('start.html')
+@app.route('/')
+def root_page():
+    surv_title = satisfaction_survey.title
+    surv_instruct = satisfaction_survey.instructions
+    return render_template('root.html', title=surv_title, instructions=surv_instruct)
+
+
+@app.route('/questions/<int:quest_num>')
+def question(quest_num):
+    surv_question = satisfaction_survey.questions[quest_num -1].question
+    surv_choices  = satisfaction_survey.questions[quest_num -1].choices
+    return render_template('question.html', question=surv_question, choices=surv_choices, quest_num=quest_num)
