055aaf91fa22daf907c89a3ae82d4601e0fccc94
nathan <nwbryant@umich.edu>
2020-07-13 15:37:00 -0700

    added handling for submitted answers

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from surveys import satisfaction_survey
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'springboardsurveyapp'
 
+toolbar = DebugToolbarExtension(app)
 responses = []
 
 @app.route('/')
@@ -17,3 +20,18 @@ def question(quest_num):
     surv_question = satisfaction_survey.questions[quest_num -1].question
     surv_choices  = satisfaction_survey.questions[quest_num -1].choices
     return render_template('question.html', question=surv_question, choices=surv_choices, quest_num=quest_num)
+
+@app.route('/answers', methods=["POST"])
+def answer():
+    surv_questions = satisfaction_survey.questions
+    answer = request.form["form-answer"]
+    responses.append(answer)
+    if len(responses) >= len(surv_questions):
+        return redirect ('/answers')
+    else:
+        next_q = len(responses) + 1
+        return redirect(f'questions/{next_q}')
+
+@app.route('/answers')
+def render_answers():
+    return f"{responses}"
