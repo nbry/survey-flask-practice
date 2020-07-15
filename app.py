3cf7ec24a462d64ede54f88620d4f9ab9a713091
nathan <nwbryant@umich.edu>
2020-07-14 18:15:28 -0700

    added flask sessions, error handling for empty answers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -7,24 +7,34 @@ app.config['SECRET_KEY'] = 'springboardsurveyapp'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
-responses = []
 
 
 @app.route('/')
 def root_page():
-    # responses = []
     surv_title = satisfaction_survey.title
     surv_instruct = satisfaction_survey.instructions
     return render_template('root.html', title=surv_title, instructions=surv_instruct)
 
+@app.route('/starting', methods=["POST"])
+def start_survey():
+    session['responses'] = []
+    return redirect('/questions/1')
+
+@app.route('/questions')
+def redirect_home():    
+    return redirect ('/')
 
 @app.route('/questions/<int:quest_num>')
 def question(quest_num):
-    if len(responses) >= len(satisfaction_survey.questions):
+    try:
+        session['responses']
+    except:
+        return redirect ('/')
+    if len(session['responses']) >= len(satisfaction_survey.questions):
         return redirect('/thank-you')
-    if len(responses) + 1 != quest_num:
+    if len(session['responses']) + 1 != quest_num:
         flash('Please follow the questions in order', 'error')
-        return redirect(f'/questions/{len(responses) + 1}')
+        return redirect(f'/questions/{len(session["responses"]) + 1}')
 
     surv_question = satisfaction_survey.questions[quest_num - 1].question
     surv_choices = satisfaction_survey.questions[quest_num - 1].choices
@@ -34,13 +44,22 @@ def question(quest_num):
 
 @app.route('/answers', methods=["POST"])
 def answer():
+    try:
+        request.form["form-answer"]
+    except:
+        flash('please choose an answer', 'error')
+        return redirect(f'/questions/{len(session["responses"]) + 1}')
+        
     surv_questions = satisfaction_survey.questions
     answer = request.form["form-answer"]
+    responses = session['responses']
     responses.append(answer)
-    if len(responses) >= len(surv_questions):
+    session['responses'] = responses
+    
+    if len(session['responses']) >= len(surv_questions):
         return redirect('/thank-you')
     else:
-        next_q = len(responses) + 1
+        next_q = len(session['responses']) + 1
         return redirect(f'questions/{next_q}')
 
 
